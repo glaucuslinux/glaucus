0924d52865a39cb3b48ce4ce1e194c27440b9c0c
Firas Khalil Khana <firasuke@gmail.com>
2018-09-01 03:27:42 +0300

    Minor changes

---
+++ scripts/initialize.sh
@@ -0,0 +1,12 @@
+#!/bin/dash -ex
+
+git config --global user.name "Firas Khalil Khana"
+git config --global user.email "firasuke@gmail.com"
+
+mkdir -v ../logs
+mkdir -v ../system
+mkdir -pv ../temporary/system/builds
+mkdir -v ../temporary/system/sources
+mkdir -pv ../temporary/toolchain/builds
+mkdir -v ../temporary/toolchain/sources
+mkdir -v ../toolchain
