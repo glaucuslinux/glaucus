f50e845c4e966ae93458f2d63383107fd2bb55d5
Firas Khalil Khana <firasuke@gmail.com>
2018-09-12 00:35:53 +0300

    Minor changes

--- scripts/system/construct.sh
+++ scripts/system/construct.sh
@@ -14,7 +14,7 @@ construct sbase ubase sinit smdev svc
 construct dash loksh
 construct mawk byacc re2c
 construct zlib mandoc
-construct e2fsprogs iproute2
+construct e2fsprogs iproute2 file
 construct bin86 lilo linux
 . /home/glaucus/scripts/system/configure.sh
 
