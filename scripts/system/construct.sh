a7ecdac11ba321c45f05f5b9d1299a64b3abc4e1
Firas Khalil Khana <firasuke@gmail.com>
2018-09-16 01:07:12 +0300

    Minor changes

--- scripts/system/construct.sh
+++ scripts/system/construct.sh
@@ -10,12 +10,13 @@ construct () {
 
 . /home/glaucus/scripts/system/prepare.sh
 construct musl
-construct sbase ubase sinit smdev svc
-construct dash loksh
-construct mawk byacc re2c
-construct zlib mandoc
-construct e2fsprogs iproute2 file diffutils
-construct bin86 lilo linux
-. /home/glaucus/scripts/system/configure.sh
+construct lobase
+#construct sbase ubase sinit smdev svc
+#construct dash loksh
+#construct mawk byacc re2c
+#construct zlib mandoc
+#construct e2fsprogs iproute2 file
+#construct bin86 lilo linux
+#. /home/glaucus/scripts/system/configure.sh
 
 # . /home/glaucus/scripts/system/img.sh
