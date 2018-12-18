8e4e2a72f41b113af68405a1374ab29b8270df98
Firas Khalil Khana <firasuke@gmail.com>
2018-12-18 05:14:09 +0200

    Minor changes

--- scripts/system/construct.sh
+++ scripts/system/construct.sh
@@ -16,7 +16,7 @@ construct dash
 construct zlib xz libarchive
 construct netbsd-curses loksh pcre2 less mandoc
 construct e2fsprogs iproute2 file opendoas libressl
-construct bin86 lilo #linux bin86 lilo
+construct linux bin86 lilo
 construct vim
 construct skalibs execline s6-portable-utils s6-linux-utils s6 s6-linux-init s6-rc
 . /home/glaucus/scripts/system/configure.sh
