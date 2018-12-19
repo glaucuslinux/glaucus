cad9b0b9592f2a00ae3f18c547d72250341be9c5
Firas Khalil Khana <firasuke@gmail.com>
2018-12-19 06:01:08 +0200

    Minor changes

--- scripts/toolchain/construct.sh
+++ scripts/toolchain/construct.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/dash -ex
-prepare () {
-        . /home/glaucus/scripts/toolchain/clean.sh
-        . /home/glaucus/scripts/variables
-        assign_basic_variables
-        . $CERD/gcc/ceras
-        tar xfv $CERD/$name/$name-$version.tar.xz -C $TSRC
-        cd $TBLD
-        mkdir -v linux binutils gcc_1 musl gcc_2
-}
 construct () {
         for item in "$@"
         do
@@ -23,5 +14,7 @@ construct () {
                 esac
         done
 }
-prepare
+. /home/glaucus/scripts/toolchain/clean.sh
+. /home/glaucus/scripts/variables
+assign_basic_variables
 construct linux binutils gmp mpc mpfr gcc_1 musl gcc_2
