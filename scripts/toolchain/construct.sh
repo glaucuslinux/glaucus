20c1a0f8fd86648804ab11e57ec7429de6019e6f
Firas Khalil Khana <firasuke@gmail.com>
2018-12-22 05:46:49 +0200

    Minor changes

--- scripts/toolchain/construct.sh
+++ scripts/toolchain/construct.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/dash -ex
-<<<<<<< HEAD
-<<<<<<< HEAD
 construct(){
-=======
-=======
->>>>>>> cad9b0b9592f2a00ae3f18c547d72250341be9c5
-construct () {
->>>>>>> cad9b0b9592f2a00ae3f18c547d72250341be9c5
         for item in "$@"
         do
                 case $item in
