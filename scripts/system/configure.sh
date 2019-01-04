71c5afba52d2e34535d918592ba82a9258a019d4
Firas Khalil Khana <firasuke@gmail.com>
2019-01-04 22:47:53 +0200

    Minor changes

--- scripts/system/configure.sh
+++ scripts/system/configure.sh
@@ -15,60 +15,37 @@ EOF
 cat > $GLAD/etc/hosts << EOF
 127.0.0.1 glaucus
 EOF
-cat > $GLAD/bin/rc.init << EOF
-#!/bin/sh
+cat > $GLAD/bin/init << EOF
+#!/bin/execlineb -P
+/bin/s6-envdir /etc/s6/env
+/bin/importas /bin /bin
+/bin/export PATH /bin
+cd /
+emptyenv -p
+s6-setsid -b
 umask 022
-/bin/mount -n -t proc -o nosuid,noexec,nodev proc /proc
-/bin/mount -n -t sysfs -o nosuid,noexec,nodev sysfs /sys
-/bin/mount -n -t tmpfs -o nosuid,mode=0755 dev /dev
-/bin/mkdir /dev/pts
-/bin/mount -n -t devpts -o gid=5,mode=0620 devpts /dev/pts
-/bin/mount -o remount,ro /
-echo /bin/smdev > /proc/sys/kernel/hotplug
-/bin/smdev -s
-cd /dev
-/bin/ln -fs /proc/self/fd/0 stdin
-/bin/ln -fs /proc/self/fd/1 stdout
-/bin/ln -fs /proc/self/fd/2 stderr
-/bin/ln -fs /proc/self/fd fd
-cd - 1>/dev/null
-/bin/mount -o remount,rw /
-/bin/mount -a
-/bin/ln -fs /proc/mounts /etc/mtab
-/bin/hostname glaucus
-/bin/ip addr add 127.0.0.1/8 dev lo broadcast + scope host
-/bin/ip link set lo up
-export TZ="Asia/Damascus"
-/bin/hwclock -s /dev/rtc0
-unset TZ
-[ -f /etc/random-seed ] && /bin/cat /etc/random-seed >/dev/urandom
-/bin/dd if=/dev/urandom of=/etc/random-seed count=1 bs=512 2>/dev/null
-/bin/svc -s
-: > /var/run/utmp
-/bin/sh -c '/bin/respawn /bin/getty /dev/tty1 linux' &>/dev/null &
+if { s6-mount -nwt tmpfs -o mode=0755 tmpfs /run }
+if { s6-hiercopy /etc/s6/run-image /run }
+emptyenv -p
+s6-envdir /etc/s6/env
+redirfd -r 0 /dev/null
+redirfd -wnb 1 /run/service/s6-svscan-log/fifo
+background
+{
+        s6-setsid
+        redirfd -w 1 /run/service/s6-svscan-log/fifo
+        fdmove -c 2 1
+        /etc/rc.init
+}
+unexport !
+fdmove -c 2 1
+s6-svscan -st0 /run/service
 EOF
-cat > $GLAD/bin/rc.shutdown << 'EOF'
-#!/bin/sh
-umask 022
-/bin/stty onlcr
-export TZ="Asia/Damascus"
-/bin/hwclock -w /dev/rtc0
-unset TZ
-/bin/dd if=/dev/urandom of=/etc/random-seed count=1 bs=512 2>/dev/null
-/bin/svc -k
-/bin/killall5 -s TERM
-/bin/killall5 -s KILL
-/bin/mount -o remount,ro /
-/bin/umount -a
-/bin/sync
-case "$1" in
-        poweroff)
-                /bin/halt -p
-                ;;
-        reboot)
-                /bin/halt -r
-                ;;
-esac
+cat > $GLAD/etc/rc.init << EOF
+#!/bin/execlineb -P
+EOF
+cat > $GLAD/etc/rc.shutdown << 'EOF'
+#!/bin/execlineb -P
 EOF
 cat > $GLAD/bin/poweroff << EOF
 #!/bin/sh
@@ -79,7 +56,7 @@ cat > $GLAD/bin/reboot << EOF
 /bin/kill -s INT 1
 EOF
 cd $GLAD/bin
-chmod 755 rc.init rc.shutdown poweroff reboot
+chmod 755 init rc.init rc.shutdown poweroff reboot
 cat > $GLAD/etc/profile << 'EOF'
 export PS1="[\033[0;44m\u@\h\033[0m:\033[1m\w\033[0m]$ "
 export HISTFILE=$HOME.loksh_history
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,3 @@
 #!/bin/dash -ex
-find /home/glaucus/cerata -name .git -execdir git pull \;
+git submodule foreach git pull
 find /home/glaucus/cerata -name .svn -execdir svn update \;
