0ec75b77add1f7ff0707a919c8e4c4edf2e1c1c0
Firas Khalil Khana <firasuke@gmail.com>
2019-02-15 23:25:33 +0200

    Minor changes

--- scripts/system/prepare.sh
+++ scripts/system/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/dash -ex
 cd $GLAD
-install -dv bin boot dev etc include lib proc run share/cerata sys tmp var var/lib var/lib/urandom
+install -dv bin boot dev etc include lib proc run share/cerata sys var var/lib var/lib/urandom var/log
 install -dvm 0750 root
 install -v $TOOL/$TUPL/lib64/libgcc_s.so.1 lib
 install -v $TOOL/$TUPL/lib64/libstdc++.so.6.0.25 lib
