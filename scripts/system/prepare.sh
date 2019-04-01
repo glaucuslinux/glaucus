ddb2911e91804b6532dbb182a1bdb252c575a50c
Firas Khalil Khana <firasuke@gmail.com>
2019-04-01 03:07:31 +0300

    Minor changes

--- scripts/system/prepare.sh
+++ scripts/system/prepare.sh
@@ -1,9 +1,9 @@
 #!/bin/dash -ex
 cd $GLAD
-install -dv bin boot dev etc lib proc run sys usr/cerata usr/include usr/share var var/lib var/lib/urandom var/log
+install -dv boot dev etc proc run sys usr/bin usr/cerata usr/include usr/lib usr/share var var/lib var/lib/urandom var/log
 install -dvm 0750 root
-install -v $TOOL/$TUPL/lib64/libgcc_s.so.1 lib
-install -v $TOOL/$TUPL/lib64/libstdc++.so.6.0.26 lib
-cd $GLAD/lib
+install -v $TOOL/$TUPL/lib64/libgcc_s.so.1 usr/lib
+install -v $TOOL/$TUPL/lib64/libstdc++.so.6.0.26 usr/lib
+cd $GLAD/usr/lib
 $STRIP libgcc_s.so.1 libstdc++.so.6.0.26
 ln -fsv libstdc++.so.6.0.26 libstdc++.so.6
