198548ef9091223d1ce916a430aec833e975b3d9
Firas Khalil Khana <firasuke@gmail.com>
2019-08-18 02:51:20 +0300

    Minor changes

--- scripts/system/envenomate.sh
+++ scripts/system/envenomate.sh
@@ -40,6 +40,7 @@ envenomate mawk byacc mawk
 
 #envenomate util-macros
 #envenomate xorgproto libXau libXdmcp xcbproto libxcb
-envenomate zlib libpng freetype_1 #harfbuzz #freetype_2 fontconfig
+#envenomate zlib libpng freetype_1 expat fontconfig
 #envenomate libxtrans libX11 libXext libFS libICE libSM libXScrnSaver libXt libXmu libXpm libXaw libXfixes libXcomposite libXrender libXcursor libXdamage libfontenc libXfont libXft libXi libXinerama libXrandr libXRes libXtst libXv libXvMC libXxf86dga libXxf86vm libdmx libpciaccess libxkbfile libxshmfence
 #envenomate libxcb-util libxcb-util-image libxcb-keysyms libxcb-render-util libxcb-wm libxcb-cursor
+envenomate pkgconf
