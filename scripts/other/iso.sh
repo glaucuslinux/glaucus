0a6a5db2ee1bec6c458bd7c32921892c8d794f44
Firas Khalil Khana <firasuke@gmail.com>
2020-01-14 15:36:36 +0200

    Add pciutils and fix eudev's sum

--- scripts/other/iso.sh
+++ scripts/other/iso.sh
@@ -11,8 +11,8 @@ $RSYNC boot etc root usr var $DEST --delete
 
 install -dv $DEST/images $DEST/isolinux $DEST/kernel
 
-$RSYNC /usr/lib/syslinux/bios/isolinux.bin $DEST/isolinux --delete
-$RSYNC /usr/lib/syslinux/bios/ldlinux.c32 $DEST/isolinux --delete
+$RSYNC /usr/lib/syslinux/bios/isolinux.bin /usr/lib/syslinux/bios/ldlinux.c32 \
+  $DEST/isolinux --delete
 $RSYNC /usr/lib/syslinux/bios/memdisk $DEST/isolinux/kernel --delete
 
 cat > $DEST/isolinux/isolinux.cfg << EOF
@@ -20,6 +20,6 @@ prompt 0
 default glaucus
 label glaucus
   kernel /boot/vmlinuz
-  append root=/dev/sr0 rootfstype=iso9660 init=/etc/s6/bin/init ro
+  append root=/dev/sda1 rootfstype=iso9660 init=/etc/s6/bin/init ro
 EOF
 mkisofs -o glaucus.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table $DEST
