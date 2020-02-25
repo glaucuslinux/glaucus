d24f005bea845abc2be691bc651cd1a13118b0cc
Firas Khalil Khana <firasuke@gmail.com>
2020-02-25 16:12:59 +0200

    Remove stupid double quotes

--- scripts/system/img.sh
+++ scripts/system/img.sh
@@ -5,35 +5,35 @@
 
 NAME=glaucus.img &&
 SIZE=2G &&
-LOOP="$(losetup -f)" &&
+LOOP=$(losetup -f) &&
 RSYNC='/usr/bin/rsync -vaHAXx' &&
 
 cd /home/glaucus &&
 
-qemu-img create -f raw "$NAME" "$SIZE" &&
+qemu-img create -f raw $NAME $SIZE &&
 dd if=scripts/other/mbr.bin of=glaucus.img conv=notrunc bs=440 count=1 &&
 
-parted -s "$NAME" mklabel msdos &&
-parted -s -a none "$NAME" mkpart primary ext4 0 "$SIZE" &&
-parted -s -a none "$NAME" set 1 boot on &&
+parted -s $NAME mklabel msdos &&
+parted -s -a none $NAME mkpart primary ext4 0 $SIZE &&
+parted -s -a none $NAME set 1 boot on &&
 
 losetup -D &&
-losetup "$LOOP" "$NAME" &&
+losetup $LOOP $NAME &&
 
-partx -a "$LOOP" &&
-mkfs.ext4 "$(printf "$LOOP")"p1 &&
+partx -a $LOOP &&
+mkfs.ext4 $(printf $LOOP)p1 &&
 
-mount "$(printf "$LOOP")"p1 /mnt/loop &&
+mount $(printf $LOOP)p1 /mnt/loop &&
 
 rm -frv /mnt/loop/lost+found &&
 
 install -dv /mnt/loop/dev /mnt/loop/proc /mnt/loop/run /mnt/loop/sys &&
-"$RSYNC" boot etc root usr var /mnt/loop --delete &&
+$RSYNC boot etc root usr var /mnt/loop --delete &&
 
 install -dv /mnt/loop/boot/extlinux &&
-"$RSYNC" scripts/other/extlinux.conf /mnt/loop/boot/extlinux &&
+$RSYNC scripts/other/extlinux.conf /mnt/loop/boot/extlinux &&
 extlinux --install /mnt/loop/boot/extlinux &&
 
 umount /mnt/loop &&
-partx -d "$LOOP" &&
-losetup -d "$LOOP"
+partx -d $LOOP &&
+losetup -d $LOOP
