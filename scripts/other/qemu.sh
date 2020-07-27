574fca82fe2da28d3b8b8a06c3206db05f88ca3d
Firas Khalil Khana <firasuke@gmail.com>
2020-07-27 23:24:12 +0300

    Move scripts to a separate radula repo

--- scripts/other/qemu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh -e
-qemu-system-x86_64 -enable-kvm -cpu host -m 64M -drive format=raw,file=/home/glaucus/glaucus.img
--- scripts/other/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-# Copyright (c) 2018-2020, Firas Khalil Khana
-# Distributed under the terms of the ISC License
-
-git submodule foreach git pull
